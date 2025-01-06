c2403c8dc380ec976de9e344d865921af6108457
MinDlessAK <akash6386184@gmail.com>
2025-01-06 11:19:23 +0530

    notification

---
+++ src/notification/dbrepo/notification.provider.ts
@@ -0,0 +1,11 @@
+import { DataSource } from 'typeorm';
+import { Notification } from './notification.repository';
+
+
+export const NotificationProviders = [
+  {
+    provide: 'NOTIFICATION_REPOSITORY',
+    useFactory: (dataSource: DataSource) => dataSource.getRepository(Notification),
+    inject: ['DATA_SOURCE'],
+  },
+];
